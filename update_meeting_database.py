a19a82b34c0130a05cb2fb8a707261a90dcefb47
Joshua Tauberer <jt@occams.info>
2013-06-30 08:41:42 -0400

    show your ANCs next meeting

--- update_meeting_database.py
+++ update_meeting_database.py
@@ -3,8 +3,9 @@
 # Store them in www/meetings.json, and update existing meetings
 # in place according to the RSS feed's guid field.
 
-import json, os.path, urllib
+import json, os.path, urllib, re
 import lxml.etree, lxml.html
+from datetime import datetime
 
 output_filename = "www/meetings.json"
 
@@ -12,6 +13,9 @@ if os.path.exists(output_filename):
 	meetings = json.load(open(output_filename))
 else:
 	meetings = []
+	
+# Clear out old entries that don't conform to current data format.
+#meetings = [m for m in meetings if "anc" in m]
 
 # Open the RSS feed.	
 dom = lxml.etree.parse(urllib.urlopen("http://anc.dc.gov/node/all/events"))
@@ -88,8 +92,26 @@ for meeting in dom.xpath("channel/item"):
 		if len(divs) > 0:
 			value = "; ".join(d.text_content().strip() for d in divs)
 			meeting_info[fieldname] = value
+			
+	# Scrape the title field to determine which ANC this is for and what kind of
+	# meeting it is.
+	m = re.match(r"ANC (\d[A-Z]) (Meeting|Monthly Meeting|Bimonthly Meetings)$", meeting_info["title"])
+	if not m:
+		print "Unrecognized meeting title format:", meeting_info["title"]
+	else:
+		meeting_info["anc"] = m.group(1)
+		meeting_info["meeting_type"] = m.group(2)
+		
+	# Parse the meeting time into the ISO datetime format.
+	meeting_info["when"] = re.sub(" to \d.*$", "", meeting_info["when"]) # if there's "time X to time Y", chop off the second time
+	meeting_info["when"] = datetime.strptime(meeting_info["when"], "%A, %B %d, %Y - %I:%M%p").isoformat()
 
 # Write out the JSON file.
 with open(output_filename, "w") as outputfile:
 	json.dump(meetings, outputfile, sort_keys=True, indent=4)
 
+# Also write out a JSONP file to make embedding in the site easier,
+with open(output_filename + "p", "w") as outputfile:
+	outputfile.write("anc_meetings = \n")
+	json.dump(meetings, outputfile, sort_keys=True, indent=4)
+	
