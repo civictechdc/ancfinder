6436ad9083423b8b49585a70196230e06415b440
Joshua Tauberer <jt@occams.info>
2013-08-24 10:05:00 -0400

    move the django sqlite database location and take it out of the repository (+.gitignore), and remove a duplicated file

--- ancbrigadesite/settings.py
+++ ancbrigadesite/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(__file__), 'data/data.db').replace('\\','/'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), 'database.sqlite').replace('\\','/'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
