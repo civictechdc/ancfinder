474c26ac7876b4119e731a0b056d07c1c29bf0b4
stvnrlly <stevenmreilly+github@gmail.com>
2013-09-08 12:53:30 -0400

    adding commissioner terms into index.html search results

--- update_terms.py
+++ update_terms.py
@@ -1,17 +1,21 @@
 import csv
-f = open('data/ancs.csv', 'rb')
-reader = csv.reader(f, delimiter=',', quotechar='"')
+reader = csv.reader(open('data/historical-commissioners.csv', 'rb'), delimiter=',', quoting=csv.QUOTE_MINIMAL)
+writer = csv.writer(open('data/anc-terms.csv', 'wb'), delimiter=',', quotechar='/') 
+
+#Not sure if I used quotechar correctly, but this got the result I was trying to achieve.
 
 terms = {}
 
 for row in reader:
-	if row[2] == 'No Candidate':
+	short = str([row[3][:3] + " " + row[2][:3]])
+	if row[2] == 'No candidate':
 		continue
-	elif row[2] in terms:
-		terms[row[2]] += 1
+	elif short in terms:
+		terms[short] += 1
+		t = terms[short]
 	else:
-		terms[row[2]] = 1
-
-print terms
-	
-f.close()
+		terms[short] = 1
+		t = 1
+	output = [row[0]] + [row[1]] + [row[2]] + [row[3]] + [t]
+	if row[0][:4] == "2012":
+		writer.writerow(output)
