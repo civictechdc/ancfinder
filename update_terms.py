eec9cb28f4085a4332ff586e6a705fc289d090dd
Joshua Tauberer <jt@occams.info>
2013-09-18 20:20:48 -0400

    sort data/anc-contestation.csv, and also read file without binary flag so it works with python3

--- update_terms.py
+++ update_terms.py
@@ -1,9 +1,9 @@
 import csv
 
-history = csv.reader(open('data/historical-commissioners.csv', 'rb'), delimiter=',', quoting=csv.QUOTE_MINIMAL)
-contestation = csv.reader(open('data/anc-candidates-2012.csv', 'rb'), delimiter=',')
-h_output = csv.writer(open('data/anc-terms.csv', 'wb'), delimiter=',', quotechar='/') 
-c_output = csv.writer(open('data/anc-contestation.csv', 'wb'), delimiter=',')
+history = csv.reader(open('data/historical-commissioners.csv', 'r'), delimiter=',', quoting=csv.QUOTE_MINIMAL)
+contestation = csv.reader(open('data/anc-candidates-2012.csv', 'r'), delimiter=',')
+h_output = csv.writer(open('data/anc-terms.csv', 'w'), delimiter=',', quotechar='/') 
+c_output = csv.writer(open('data/anc-contestation.csv', 'w'), delimiter=',')
 
 #Not sure if I used quotechar correctly, but this got the result I was trying to achieve.
 
@@ -32,7 +32,7 @@ for row in contestation:
 	else:
 		candidates[row[1]] = 1
 
-for total in candidates:
+for total in sorted(candidates):
 	output = [total] + [candidates[total]]
 	if len(total) == 4:
 		c_output.writerow(output)
