ffeaf685099bd44c8744652517b4c95b558e0d52
stvnrlly <stevenmreilly+github@gmail.com>
2013-10-14 15:37:43 -0400

    download ABRA locations and convert to geojson; needs to be merged into main update file

--- update_crimes.py
+++
@@ -1,9 +0,0 @@
-import csv, urllib2, zipfile, StringIO
-
-zipstream = urllib2.urlopen("http://data.octo.dc.gov/feeds/crime_incidents/archive/crime_incidents_2013_CSV.zip")
-buf = StringIO.StringIO()
-buf.write(zipstream.read())
-buf.seek(0)
-
-zip = zipfile.ZipFile(buf)
-csvfile = csv.reader(zip.open("the_csv.csv"))
