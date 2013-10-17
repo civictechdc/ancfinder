4b08251c4d6984966042ebb7cab9748c8fcbe451
stvnrlly <stevenmreilly+github@gmail.com>
2013-10-16 21:04:16 -0400

    add liquor licenses

--- update_abra.py
+++ update_abra.py
@@ -37,24 +37,35 @@ subprocess.call('gdaltransform -s_srs EPSG:26985 -t_srs EPSG:4326 < data/abra-na
 
 # Check coordinates against Govtrack GIS
 abra_latlong = csv.reader(open('data/abra-latlong.csv'), delimiter=' ')
-abra_licenses = csv.writer(open('data/abra-licenses.csv', 'w'), delimiter=',')
+smd_abra_licenses = csv.writer(open('data/smd-abra-licenses.csv', 'w'), delimiter=',')
+anc_abra_licenses = csv.writer(open('data/anc-abra-licenses.csv', 'w'), delimiter=',')
 
-licenses = {}
+smd_licenses = {}
+anc_licenses = {}
 
 for rec in abra_latlong:
 	url = "http://gis.govtrack.us/boundaries/dc-smd-2013/?contains=" + rec[1] + "," + rec[0]
 	request = requests.get(url)
 	data = json.loads(request.text)
 	smd = data['objects'][0]['external_id']
+	anc = data['objects'][0]['external_id'][:2]
 	print smd, rec[0], rec[1]
-	if smd in licenses:
-		licenses[smd] += 1
+	if smd in smd_licenses:
+		smd_licenses[smd] += 1
 	else:
-		licenses[smd] = 1
+		smd_licenses[smd] = 1
+	if anc in anc_licenses:
+		anc_licenses[anc] += 1
+	else:
+		anc_licenses[anc] = 1
 
-for rec in sorted(licenses):
-	output = [rec] + [licenses[rec]]
-	abra_licenses.writerow(output)
+for rec in sorted(smd_licenses):
+	output = [rec] + [smd_licenses[rec]]
+	smd_abra_licenses.writerow(output)
+
+for rec in sorted(anc_licenses):
+	output = [rec] + [anc_licenses[rec]]
+	anc_abra_licenses.writerow(output)
 
 # Clean up extra files
 subprocess.call('rm -rf abra abra.zip abra-nad83.csv abra-latlong.csv', shell=True)
