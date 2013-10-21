7034f3235385ebbb54b6aaff5a2007aab2933aab
stvnrlly <stevenmreilly+github@gmail.com>
2013-10-21 13:47:19 -0400

    cleaning up ABRA data

--- ancbrigadesite/views.py
+++ ancbrigadesite/views.py
@@ -54,7 +54,7 @@ def anc_info(request, anc):
 		{ "key": "B01002_001E",	"label": "median age" },
 		{ "key": "B19019_001E",	"label": "median household income", "details": "", "is_dollars": True },
 		{ "key": "POP_DENSITY",	"label": "density (pop/sq-mi)", "details": "Total population divided by the area of the ANC." },
-		{ "key": "liquor_licenses",	"label": "liquor licenses",	"details": "Liquor licenses held in the area" },
+		{ "key": "liquor_licenses",	"label": "liquor licenses",	"details": "Liquor licenses held by bars and restaurants in the area" },
 	]
 	for s in census_stats:
 		s["value"] = info["census"][s["key"]]["value"]
