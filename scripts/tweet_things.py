7dd7905942c5a0c3d7809a0498993f4287abd9b4
mficke <matthew.ficke@gmail.com>
2015-01-29 22:07:07 -0500

    Modify script to not tweet if document is empty.

--- scripts/tweet_things.py
+++ scripts/tweet_things.py
@@ -109,6 +109,9 @@ def post_document_tweet(previous_tweets):
 
 	for doc in Document.objects.filter(created__gt=now() - timedelta(days=5)).order_by('created'):
 		key = "document:%d" % doc.id
+		# Don't post empty documents
+		if document.annotation_document.body.strip() == "":
+			break
 		if key in previous_tweets: continue
 
 		post_tweet(
