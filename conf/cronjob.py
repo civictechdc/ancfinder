c49df5ef5c80c3ccd0798b74e2adcac8c6adf8a7
Joshua Tauberer <jt@occams.info>
2015-03-28 16:05:39 -0400

    revise instructions on how to submit commissioner info; drop link to our old google doc; no need to update ancs.json in cron job since we don't use the base data on commissioners anymore

--- conf/cronjob.py
+++ conf/cronjob.py
@@ -6,5 +6,4 @@ os.environ["STATIC_ROOT"] = env["STATIC_ROOT"]
 
 # Run da scripts.
 os.system(". .env/bin/activate; python scripts/scrape_anc.py")
-os.system(". .env/bin/activate; python3 scripts/update_anc_database.py --base")
 os.system(". .env/bin/activate; python scripts/update_meeting_times.py")
