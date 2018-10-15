b4ab6086297e108316e16f4437a50e1651f6cac2
Michael Kalish <kalishmichael@gmail.com>
2018-10-15 12:43:12 -0400

    Version 2 kick off with docker support

--- ancfinder_site/ext/django-registration-pv/emailverification/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import *
-
-urlpatterns = patterns('',
-    (r'^code/([0-9A-Z]+)$', 'emailverification.views.processcode'),
-    (r'^code/delete/([0-9A-Z]+)$', 'emailverification.views.killcode'),
-    (r'^ping/([a-zA-Z]+)$', 'emailverification.views.emailping'),
-)
-
