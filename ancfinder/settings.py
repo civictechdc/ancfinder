911c28a401b8754dc3d46745a5c47404c1d41fdd
Michael Kalish <kalishmichael@gmail.com>
2018-10-15 12:58:19 -0400

    Making Mapbox API Key and env variable

--- ancfinder/settings.py
+++ ancfinder/settings.py
@@ -15,6 +15,7 @@ import os, sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 environment_file = '/srv/app/environment.yaml'
+local_environment_file = 'user_local_environment.yaml'
 
 if os.path.exists(environment_file):
     print("Running production deployment...")
@@ -53,7 +54,47 @@ if os.path.exists(environment_file):
     # in apps' "static/" subdirectories and in STATICFILES_DIRS.
     # Example: "/home/media/media.lawrence.com/static/"
     STATIC_URL = env["STATIC_ROOT"]
+    MAPBOX_API_KEY = env["MAPBOX_API_KEY"]
 
+elif os.path.exists(local_environment_file):
+    print("Found local environemnt yaml")
+
+    import yaml
+    with open(local_environment_file) as f:
+      env = yaml.load(f)
+    SECRET_KEY = env['DJANGO_SECRET_KEY']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.path.dirname(__file__), 'database.sqlite').replace('\\','/'),
+        }
+    }
+
+    SECRET_KEY = '4k90fdv_y#5na2wul&fcd&xg23va!7#3h)we)6%c2ma8mt0uc-'
+    DEBUG = True
+
+    ALLOWED_HOSTS = ["*"]
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    ADMINS = env['ADMINS']
+    MANAGERS = ADMINS
+
+    # EMAIL_HOST = env['SMTP_HOST']
+    # EMAIL_HOST_USER = env['SMTP_USER']
+    # EMAIL_HOST_PASSWORD = env['SMTP_PASSWORD']
+    # EMAIL_USE_TLS = True
+
+    ALLOWED_HOSTS = ["*"] # anything unexpected will be filtered out by the http server
+
+    OPENID_TEMP_FOLDER = "/tmp/openid-ancfinder"
+
+    # Absolute path to the directory static files should be collected to.
+    # Don't put anything in this directory yourself; store your static files
+    # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+    # Example: "/home/media/media.lawrence.com/static/"
+    STATIC_URL = env["STATIC_ROOT"]
+    MAPBOX_API_KEY = env["MAPBOX_API_KEY"]
 else:
     print("Running development deployment...")
 
@@ -61,6 +102,7 @@ else:
     DEBUG = True
 
     ALLOWED_HOSTS = ["*"]
+
 
     DATABASES = {
         'default': {
